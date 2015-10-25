97a3c11366ea17e66e3247ba00d80ff46ca10bd6
Rob Lourens <roblou@microsoft.com>
2015-10-25 14:14:27 -0700

    Add config option to search the word under the cursor when there's no selection

--- extension.ts
+++ extension.ts
@@ -11,21 +11,38 @@ export function activate() {
 	});
 	
 	function getQueryAndSearch(searchUrl: string, providerName: string): void {
-		const editor = vscode.window.getActiveTextEditor();
-		if (!editor) return;
-		
-		const selection = editor.getSelection();
-		let searchTextP: Thenable<string>;
-		if (selection.isEmpty()) {
-			searchTextP = vscode.window.showInputBox({ prompt: `${providerName}: Enter your query` });
-		} else {
-			searchTextP = Promise.resolve(editor.getTextDocument().getTextInRange(new vscode.Range(selection.anchor, selection.active)));
-		}
-		
-		searchTextP.then(searchText => {
+		getQuery(searchUrl, providerName).then(searchText => {
 			if (searchText) {
 				open(searchUrl.replace('%s', encodeURIComponent(searchText)));
 			}
 		});
+	}
+	
+	function getQuery(searchUrl: string, providerName: string): Thenable<string> {
+		const editor = vscode.window.getActiveTextEditor();
+		if (!editor) return Promise.resolve(null);
+		
+		return vscode.extensions.getConfigurationMemento('searchdocs').getValue('searchWordUnderCursor', false).then(searchWordUnderCursor => {
+			const selection = editor.getSelection();
+			if (searchWordUnderCursor || !selection.isEmpty()) {
+				let searchText = getSelectedTextOrCursorWord(editor).trim();
+				if (searchText) return searchText;
+			}
+			
+			return vscode.window.showInputBox({ prompt: `${providerName}: Enter your query` });
+		}).then(searchText => searchText && searchText.trim());	
+	}
+	
+	function getSelectedTextOrCursorWord(editor: vscode.TextEditor): string {
+		const selection = editor.getSelection();
+		const doc = editor.getTextDocument();
+		if (selection.isEmpty()) {
+			const cursorWordRange = doc.getWordRangeAtPosition(selection.active);
+			return cursorWordRange ?
+				doc.getTextInRange(cursorWordRange) :
+				'';
+		} else {
+			return doc.getTextInRange(selection);
+		}
 	}
 }
